6ebc43ea7b319b53d22b40e9678bc1034c892641
Marnix47 <maxome7@icloud.com>
2022-10-07 17:56:21 +0000

    dag 3 deel 1 voltooid

--- day-3/script.py
+++ day-3/script.py
@@ -1,16 +1,41 @@
 inputFile = open("input.txt", "r")
 lines = inputFile.readlines()
 
-totalAppearances = 0
+tA = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 for line in lines:
-    i = 0
-    for i in line:
-        newLine = int(line[i])
-        print(newLine)
-        if newLine == 0:
-            totalApperances = totalAppearances + 10 ** i
+        #print(line)
+        #print("started with new line")
+        i = 0
+        while (i < 12):
+            newLine = int(line[i])
+            #print(newLine)
+            if newLine == 1:
+                tA[i] = tA[i] + 1
             i = i + 1
+        i = 0
 
-print(totalAppearances)
+print(tA)
+
+gammaRate = ""
+epsilonRate = ""
+
+t = 0
+
+while (t < 12):
+    if tA[t] > 500:
+        gammaRate = gammaRate + "1"
+        epsilonRate = epsilonRate + "0"
+    if tA[t] < 500:
+        gammaRate = gammaRate + "0"
+        epsilonRate = epsilonRate + "1"
+    t = t + 1
+
+print("epsilonRate is " + epsilonRate)
+print("gammaRate is " + gammaRate)
+
+testNumber = int(epsilonRate, 2) * int(gammaRate, 2)
+
+print(testNumber)
+
         
